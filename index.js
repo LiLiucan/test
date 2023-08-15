1a8c190912b7365e6d5aa97a25e5a8dc2b9e90c6
李留灿 <liliucan@wanda.cn>
2023-08-15 13:58:13 +0800

    Add files via upload
    web-mobile-002

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-/**
- * 设置LayaNative屏幕方向，可设置以下值
- * landscape           横屏
- * portrait            竖屏
- * sensor_landscape    横屏(双方向)
- * sensor_portrait     竖屏(双方向)
- */
-window.screenOrientation = "sensor_landscape";
+System.register(["./application.js"], function (_export, _context) {
+  "use strict";
 
-//-----libs-begin-----
-loadLib("libs/laya.core.js")
-loadLib("libs/laya.ui.js")
-loadLib("libs/laya.physics.js")
-//-----libs-end-------
-loadLib("js/bundle.js");
+  var Application, canvas, $p, bcr, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_applicationJs) {
+      Application = _applicationJs.Application;
+    }],
+    execute: function () {
+      canvas = document.getElementById('GameCanvas');
+      $p = canvas.parentElement;
+      bcr = $p.getBoundingClientRect();
+      canvas.width = bcr.width;
+      canvas.height = bcr.height;
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
